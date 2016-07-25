603bbbded7f8b03ee9f8fe53814a5894b1c1c2a4
邹国华 <zgh07280510@126.com>
2016-07-25 19:07:59 +0800

    创建闪屏页

---
+++ app/src/main/java/com/lanou/bestbeautifulthings/splash/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.lanou.bestbeautifulthings.splash;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by zouguohua on 16/7/25.
+ */
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
