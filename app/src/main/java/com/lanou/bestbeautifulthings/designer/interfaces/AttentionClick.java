494c555d7a43967f2ccf00dc84b518d4f4dea9c6
dllo <331941944@qq.com>
2016-08-04 19:05:24 +0800

    no message

---
+++ app/src/main/java/com/lanou/bestbeautifulthings/designer/interfaces/AttentionClick.java
@@ -0,0 +1,10 @@
+package com.lanou.bestbeautifulthings.designer.interfaces;
+
+import butterknife.OnClick;
+
+/**
+ * Created by dllo on 16/8/4.
+ */
+public interface AttentionClick {
+ void onClick(int position);
+}
